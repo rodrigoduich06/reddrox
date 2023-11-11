de9990e223f48e534ce0ee7566f4cf6f39a6fd96
rodrigoduich06 <150543321+rodrigoduich06@users.noreply.github.com>
2023-11-11 23:36:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ say('Mystic - Bot\nWhatsApp Bot MD', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']});
-say(`Bot creado por Bruno Sobrino`, {
+say(`Bot REDDRox`, {
   font: 'console',
   align: 'center',
   gradient: ['red', 'magenta']});
